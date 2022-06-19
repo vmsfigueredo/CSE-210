38804a60cf18cffbb84c4f348a315b2b6ff303c5
vitorfigueredo <vmsfigueredo@gmail.com>
2022-06-19 02:05:29 -0300

    Tic-Tac-Toe Updated

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -21,9 +21,9 @@ def game():
 def draw(board):
     print(f"""
         {board[0]}|{board[1]}|{board[2]}
-        --+--+--
+        -+-+-
         {board[3]}|{board[4]}|{board[5]}
-        --+--+--
+        -+-+-
         {board[6]}|{board[7]}|{board[8]}
     """)
     return
